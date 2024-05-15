0dcaf0a3dd427d53ac44c82f0547f5a963675498
Renan <silva.renanleite@gmail.com>
2024-05-15 03:19:00 -0300

    feat: usign cli to set database and theme

--- srcs/requirements/wordpress/tool/script.sh
+++ srcs/requirements/wordpress/tool/script.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/env bash
 
-if [ -f /var/www/wordpress/wp-config-sample.php ]; then
-	rm -rf /var/www/wordpress/wp-config-sample.php
+wp --allow-root config create \
+	--dbname="$DB_NAME" \
+	--dbuser="$ADMIN_NAME" \
+	--dbpass="$ADMIN_PASSWORD" \
+	--dbhost=mariadb \
+	--dbprefix="wp_"
 
-	wp --allow-root config create \
-		--dbname="$DB_NAME" \
-		--dbuser="$ADMIN_NAME" \
-		--dbpass="$ADMIN_PASSWORD" \
-		--dbhost=mariadb \
-		--dbprefix="wp_"
+wp core install --allow-root \
+	--path=/var/www/wordpress \
+	--title="42SP InceptionHELL" \
+	--url=$DOMAIN \
+	--admin_user=$ADMIN_NAME \
+	--admin_password=$ADMIN_PASSWORD \
+	--admin_email=$ADMIN_EMAIL
 
-	wp core install --allow-root \
-		--path=/var/www/wordpress \
-		--title="42SP InceptionHELL" \
-		--url=$DOMAIN \
-		--admin_user=$ADMIN_NAME \
-		--admin_password=$ADMIN_PASSWORD \
-		--admin_email=$ADMIN_EMAIL
+wp user create --allow-root	\
+	--path=/var/www/wordpress \
+	"$USER_NAME" "$USER_EMAIL" \
+	--user_pass=$USER_PASSWORD \
+	--role='author'
 
-	wp user create --allow-root	\
-		--path=/var/www/wordpress \
-		"$USER_NAME" "$USER_EMAIL" \
-		--user_pass=$USER_PASSWORD \
-		--role='author'
-fi
+# Activate the Twenty Twenty-Two theme.
+wp --allow-root theme activate twentytwentytwo
 
 exec php-fpm7.4 -F
